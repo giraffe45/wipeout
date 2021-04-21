5d3cebd906da150b216c4881879cce6fb8cb940b
giraffe45 <burden1@gmx.de>
2021-04-21 22:16:41 +0200

    example project commit

---
+++ src/test/java/htw/berlin/wipeout/WipeoutApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.wipeout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WipeoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
