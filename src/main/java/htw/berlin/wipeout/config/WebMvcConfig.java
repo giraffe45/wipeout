88707f2207e523b68b6e4ce124593095959d4613
KarimBauer <karim-bauer@hotmail.com>
2021-06-22 20:36:29 +0200

    try for heroku

--- src/main/java/htw/berlin/wipeout/config/WebMvcConfig.java
+++ src/main/java/htw/berlin/wipeout/config/WebMvcConfig.java
@@ -1,11 +1,14 @@
 package htw.berlin.wipeout.config;
 
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
